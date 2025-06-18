0c6479ae921257efd8a0f90c2b6f18b4070b20a7
RAAFID AFRAAZ G <126003403+RaafidAfraazG@users.noreply.github.com>
2025-06-18 12:34:30 +0530

    Create lostpixel.config.js

---
+++ lostpixel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'page',
+  pageShots: {
+    pages: [
+      { path: '/', name: 'homepage' }
+    ],
+    baseUrl: 'https://raafidafraazg.github.io/lost-pixel-visual-test/'
+  },
+  generateOnly: true,
+  failOnDifference: false
+};
